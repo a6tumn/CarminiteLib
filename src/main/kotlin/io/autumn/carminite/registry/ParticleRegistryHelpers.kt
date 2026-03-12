e3635da92920e3f0b446493075b9997fb73e4cc1
autumn <shinigami7x@proton.me>
2026-03-12 17:56:30 -0400

    Added new particle registry helper

--- src/main/kotlin/io/autumn/carminite/registry/ParticleRegistryHelpers.kt
+++ src/main/kotlin/io/autumn/carminite/registry/ParticleRegistryHelpers.kt
@@ -2,7 +2,10 @@
 
 package io.autumn.carminite.registry
 
+import net.fabricmc.fabric.api.client.particle.v1.ParticleProviderRegistry
 import net.minecraft.core.Registry
+import net.minecraft.core.particles.ParticleOptions
+import net.minecraft.core.particles.ParticleType
 import net.minecraft.core.particles.SimpleParticleType
 import net.minecraft.core.registries.BuiltInRegistries
 import net.minecraft.resources.Identifier
@@ -16,3 +19,11 @@ fun registerGenericParticle(
             namespaceAndPath,
             particleType
         )
+
+fun <T : ParticleOptions> registerGenericParticleProvider(
+    particleType: ParticleType<T>,
+    pendingParticleProvider: ParticleProviderRegistry.PendingParticleProvider<T>
+) {
+    ParticleProviderRegistry.getInstance().register(particleType, pendingParticleProvider)
+}
+
